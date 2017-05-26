bc135c856c6f81b4f60b1aa0b9b8c7746e006de5
kenchan20110 <kenchan20110@gmail.com>
2017-05-26 13:50:10 +0800

    basic file

---
+++ src/Geolocation.js
@@ -0,0 +1,39 @@
+﻿
+
+var initialLocation;
+var hk = new google.maps.LatLng(22.38,114.10);
+var browserSupportFlag =  new Boolean();
+
+function initialize() {
+  var myOptions = {
+    zoom: 17,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      map.setCenter(initialLocation);
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+  
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("地圖定位失敗");
+    } else {
+      alert("您的瀏覽器不支援定位服務");
+    }
+	initialLocation = hk;
+    map.setCenter(initialLocation);
+  }
+}
