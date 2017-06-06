6d32eca4d62abd2415e527fc6656e677b2726d60
kenchan20110 <kenchan20110@gmail.com>
2017-06-07 03:10:37 +0800

    update

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+// home page
+router.get('/', function(req, res, next) {
+
+    var db = req.con;
+    var data = "";
+
+    db.query('SELECT * FROM Route,RStop WHERE ROUTE_ID = Route_ROUTE_ID', function(err, rows) {
+        if (err) {
+            console.log(err);
+        }
+        var data = rows;
+
+        // use index.ejs
+        res.render('index', { title: 'Route Information', data: data});
+    });
+
+});
+
+module.exports = router;
